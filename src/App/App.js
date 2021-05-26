ca57ecb422850887a9296dc10abeb714d7313f53
Pat Findley <patfindley@gmail.com>
2021-05-26 11:30:44 -0400

    Fetch reservations

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import fetchReservations from '../ApiCalls.js'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+
+  }
+
+  componentDidMount = async() => {
+    await fetchReservations()
+    .then(data => this.setState({reservations: data}))
+    console.log(this.state.reservations);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +25,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
